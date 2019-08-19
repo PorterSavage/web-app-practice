c8df16b4f0554797c38a430d576cfa33f557c474
Porter Savage <theportersavage@gmail.com>
2019-08-18 21:20:15 -0700

    Created beginning project environment

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <div>
+                <style jsx>
+                    {`
+                    `}
+                </style>
+            </div>
+            <div>
+                <h1>Website Practice</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
